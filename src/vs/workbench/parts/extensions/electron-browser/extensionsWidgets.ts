8d561a870405ee752d12141ff937dee499e07b7a
Sofian Hnaide <sofianal@microsoft.com>
2016-03-22 15:44:28 -0700

    adding telemetry to extension widget

--- src/vs/workbench/parts/extensions/electron-browser/extensionsWidgets.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsWidgets.ts
@@ -20,6 +20,7 @@ import { UninstallAction } from 'vs/workbench/parts/extensions/electron-browser/
 import { IExtensionsService, commandCategory, IExtension, IExtensionManifest } from 'vs/workbench/parts/extensions/common/extensions';
 import { IQuickOpenService } from 'vs/workbench/services/quickopen/common/quickOpenService';
 import { getOutdatedExtensions } from 'vs/workbench/parts/extensions/common/extensionsUtil';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 
 interface IState {
 	errors: IMessage[];
@@ -50,7 +51,8 @@ export class ExtensionsStatusbarItem implements statusbar.IStatusbarItem {
 		@IMessageService private messageService: IMessageService,
 		@IExtensionsService protected extensionsService: IExtensionsService,
 		@IInstantiationService protected instantiationService: IInstantiationService,
-		@IQuickOpenService protected quickOpenService: IQuickOpenService
+		@IQuickOpenService protected quickOpenService: IQuickOpenService,
+		@ITelemetryService protected telemetrService: ITelemetryService
 	) {}
 
 	render(container: HTMLElement): IDisposable {
@@ -100,11 +102,14 @@ export class ExtensionsStatusbarItem implements statusbar.IStatusbarItem {
 
 	private onClick(): void {
 		if (this.hasErrors) {
+			this.telemetrService.publicLog('extensionWidgetClick', {mode : 'hasErrors'});
 			this.showErrors(this.state.errors);
 			this.updateState({ errors: [] });
 		} else if (this.hasUpdates) {
+			this.telemetrService.publicLog('extensionWidgetClick', {mode : 'hasUpdate'});
 			this.quickOpenService.show(`ext update `);
 		} else {
+			this.telemetrService.publicLog('extensionWidgetClick', {mode : 'none'});
 			this.quickOpenService.show(`>${commandCategory}: `);
 		}
 	}
